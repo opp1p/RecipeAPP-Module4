f46af3338fea8286681459e04a5fe4cbc061cdce
Filippo <f.monti93@gmail.com>
2021-10-09 19:51:52 +0200

    -build ui featured view -tabview for scrollable element -if cycle for featured properties -geometry reader for layout

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 struct RecipeFeaturedView: View {
     @EnvironmentObject var model:RecipeModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 0){
+            Text("Featured Recipes").font(.largeTitle).fontWeight(.bold).padding(.leading, 10).padding(.top, 20)
+        GeometryReader { geo in
+            TabView{
+                ForEach (0..<model.recipes.count) { index in
+                    if model.recipes[index].featured {
+                    VStack(alignment: .leading){
+                        
+                        ZStack{
+                            Rectangle().foregroundColor(.white)
+                            VStack{
+                            Image(model.recipes[index].image).resizable().aspectRatio(contentMode: .fill)
+                        Text(model.recipes[index].name).foregroundColor(.black).padding(5)
+                    }
+                }.frame(width: geo.size.width - 40, height: geo.size.height - 190, alignment: .center).clipped().cornerRadius(10).shadow(radius: 10, x:-5 , y:5)
+                        Text("Highlights").font(.headline)
+                        Text("xxxx, xxxx")
+                        Text("Time").font(.headline)
+                        Text("xxxx, xxxx")
+                        
+                            }
+                        }
+                    }
+            }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic)).indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeFeaturedView()
+        RecipeFeaturedView().environmentObject(RecipeModel())
     }
 }
